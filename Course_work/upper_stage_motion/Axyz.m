14bdd58d6c0fed1d4e7f7136755590f631ef1532
Vadim Yudintsev <vadim@iMac-Vadim.local>
2018-11-12 21:40:02 +0400

    v2018

--- Course_work/upper_stage_motion/Axyz.m
+++
@@ -1,6 +0,0 @@
-function A = Axyz(a1, a2, a3)
-
-A = Ax(a1)*Ay(a2)*Az(a3);
-
-end
-
--- problems/term_I/orbital_stage/matlab_example_1/draw_upper_stage.m
+++ problems/term_I/orbital_stage/matlab_example_1/draw_upper_stage.m
@@ -1,13 +1,13 @@
 function draw_upper_stage(bc, rc, A)
 
 % Nozzle 
-L1 = 1; R11 = 1; R12 = 0.3;
+L1 = 0.7; R11 = 0.8; R12 = 0.5;
 [x,y,z,c] = get_frustum(R11,R12,L1,[1,0,0]);
 [x,y,z  ] = affine_transform(x-bc(1), y-bc(2), z-bc(3), rc, A);
 surf(x,y,z,c);
 
 % Rear
-L2 = 1; R21 = 1.5; R22 = 2;
+L2 = 0.5; R21 = 0.8; R22 = 1.5;
 [x,y,z,c] = get_frustum(R21,R22,L2,[0.5,0.5,0.5]);
 x = x + L1;
 [x,y,z  ] = affine_transform(x-bc(1), y-bc(2), z-bc(3), rc, A);
@@ -15,21 +15,21 @@ surf(x,y,z,c);
 fill3(x(1,:),y(1,:),z(1,:),c(1,:,:));
 
 % Body
-L3 = 6; R3 = 2;
+L3 = 6; R3 = 1.5;
 [x,y,z,c] = get_frustum(R3,R3,L3,[0.0,0.5,0.0]);
 x = x + L1 + L2;
 [x,y,z  ] = affine_transform(x-bc(1), y-bc(2), z-bc(3), rc, A);
 surf(x,y,z,c);
 fill3(x(2,:),y(2,:),z(2,:),c(2,:,:));
 
-% Pho
-L4 = 1; R41 = 2; R42 = 2.5;
+% Adapter
+L4 = 0.5; R41 = 1.5; R42 = 0.7;
 [x,y,z,c] = get_frustum(R41,R42,L4,[0.5,1.0,0.5]);
 x = x + L1 + L2 + L3;
 [x,y,z  ] = affine_transform(x-bc(1), y-bc(2), z-bc(3), rc, A);
 surf(x,y,z,c);
 
-% Оси
+% Axes
 xc = A*[0, 7;
         0, 0;
         0, 0] + repmat(rc,1,2);
