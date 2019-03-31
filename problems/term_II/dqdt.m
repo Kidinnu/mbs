2b0cb8b720aaa00452d349259d394dbfc3ca30dc
kidinnu <vadim@mc-kidinnu.local>
2019-03-31 17:24:36 +0400

    Template

--- problems/term_II/dqdt.m
+++ problems/term_II/dqdt.m
@@ -11,15 +11,16 @@ dphi = @(i) q(p.iq(i,1)+p.N:p.iq(i,1)+p.iq(i,2)+p.N-1);
 pT = zeros(3,p.N,p.n);
 i = 1;
 for ib=1:p.n
-    pblock = p.p(phi(ib),dphi(ib));
+    pblock = p.p{ib}(phi(ib));
     for ia = 1:p.na(ib)
         for k = 1:p.n
-            pT(i,k) = pblock(:,ia)*p.T(ib,k);                    
+            pT(:,i,k) = pblock(:,ia)*p.T(ib,k);                    
         end
         i = i + 1; 
     end    
 end
 
+dq = pT(1,:,:);
 
 
 end
--- problems/term_II/main.m
+++ problems/term_II/main.m
@@ -88,6 +88,8 @@ for i=1:p.n
     end
 end
 
+squeeze( dqdt(0,[1;2;3;4;5;6;1;2;3;4;5;6],p) )
+
 
 %%
 
