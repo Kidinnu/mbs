843f0cccd363d7b5da53e4a419e21274ba2e5f1f
kidinnu <vadim@mc-kidinnu.local>
2019-03-31 17:07:37 +0400

    Template

--- problems/term_II/dqdt.m
+++ problems/term_II/dqdt.m
@@ -1,17 +1,25 @@
 function dq = dqdt(t,q,p)
 
+% Slice functions:
+% get phi array by hinge index
+phi  = @(i) q(p.iq(i,1):p.iq(i,1)+p.iq(i,2)-1);
+% get dphi array by hinge index
+dphi = @(i) q(p.iq(i,1)+p.N:p.iq(i,1)+p.iq(i,2)+p.N-1);
 
 
-
-
+% p*T matrix
 pT = zeros(3,p.N,p.n);
-
-for i=1:p.n
-   for j=1:p.n
-      pT(:,i,j) = p.p{i}()*p.T(i,j);       
-   end
-    
+i = 1;
+for ib=1:p.n
+    pblock = p.p(phi(ib),dphi(ib));
+    for ia = 1:p.na(ib)
+        for k = 1:p.n
+            pT(i,k) = pblock(:,ia)*p.T(ib,k);                    
+        end
+        i = i + 1; 
+    end    
 end
+
 
 
 end
--- problems/term_II/main.m
+++ problems/term_II/main.m
@@ -1,12 +1,14 @@
 
 p.n  = 3;
+p.na = [3;2;1];
 
 p.S0 = [1 0 0];
-
 p.S  = [-1  1  1
          0 -1  0
          0  0 -1];
 
+p.N  = sum(p.na);
+p.iq = [(cumsum(p.na)-p.na+1) p.na];
 p.T  = inv(p.S);  
 
 % Joint vectors
@@ -20,7 +22,7 @@ p.C(:,3,3)  = [-1; 0;0];
 % Masses
 p.mass = [1;1;1];
 % Inertia tensors
-p.I    = cells(p.n,1);
+p.I    = cell(p.n,1);
 p.I{1} = diag([1,1,1]);
 p.I{2} = diag([1,1,1]);
 p.I{3} = diag([1,1,1]);
@@ -53,29 +55,29 @@ Ay = @(x) [cos(x) 0 sin(x); 0 1 0; - sin(x) 0 cos(x)];
 Az = @(x) [cos(x) -sin(x) 0; sin(x) cos(x) 0; 0 0 1];
 
 % 0-1 joint (spherical)
-p.A{1} = @(q11,q12,q13) Az(q11)*Ax(q12)*Az(q13);
+p.A{1} = @(q) Az(q(1))*Ax(q(2))*Az(q(3));
 % 1-2 joint (universal)
-p.A{2} = @(q21,q22) Az(q21)*Ax(q22);
+p.A{2} = @(q) Az(q(1))*Ax(q(2));
 % 2-3 joint (cylindrical)
-p.A{3} = @(q31) Ax(q31);
+p.A{3} = @(q) Ax(q(1));
 
 % p-vectors
 % 0-1 joint: 3 x 3
-p.p{1} = @(q11,q12,q13) [p.A{1}(q11,q12,q13)*[0;0;1] p.A{1}(0,q12,q13)*[1;0;0] [0;0;1]];
+p.p{1} = @(q) [p.A{1}(q)*[0;0;1] p.A{1}([0,q(2),q(3)])*[1;0;0] [0;0;1]];
 % 1-2 joint: 3 x 2
-p.p{2} = @(q21,q22) [p.A{2}(q21,q22)*[0;0;1] [1;0;0]];
+p.p{2} = @(q) [p.A{2}(q)*[0;0;1] [1;0;0]];
 % 2-3 joint: 3 x 1
-p.p{3} = @(q31) [1;0;0];
+p.p{3} = @(q) [1;0;0];
 
 % Relative angular velocity
-p.Wr{1} = @(q11,q12,q13,dq11,dq12,dq13)  p.p{1}(q11,q12,q13)*[dq11;dq12;dq13];
-p.Wr{2} = @(q21,q22,dq21,dq22)  p.p{2}(q21,q22)*[dq21;dq22];
-p.Wr{3} = @(q31,dq31)  p.p{3}(q31)*dq31;
+p.Wr{1} = @(q,dq)  p.p{1}(q)*[dq(1);dq(2);dq(3)];
+p.Wr{2} = @(q,dq)  p.p{2}(q)*[dq(1);dq(2)];
+p.Wr{3} = @(q,dq)  p.p{3}(q)*dq;
 
 % Partial derivatives of p vectors
-p.pw{1} = @(q11,q12,q13,dq11,dq12,dq13) cross(p.Wr{1}(q11,q12,q13,dq11,dq12,dq13),p.p{1}(q11,q12,q13));
-p.pw{2} = @(q21,q22,dq21,dq22) cross(p.Wr{2}(q21,q22,dq21,dq22),p.p{2}(q21,q22)); 
-p.pw{3} = @(q31,dq31) cross(p.Wr{3}(q31,dq31),p.p{3}(q31)); 
+p.pw{1} = @(q,dq) cross(p.Wr{1}(q,dq),p.p{1}(q));
+p.pw{2} = @(q,dq) cross(p.Wr{2}(q,dq),p.p{2}(q)); 
+p.pw{3} = @(q,dq) cross(p.Wr{3}(q,dq),p.p{3}(q)); 
 
 % Diagonal blocks of K tensor in i-CS
 p.Kii = cell(p.n,1);
