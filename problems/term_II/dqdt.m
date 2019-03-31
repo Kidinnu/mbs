e4c868a9aabb228a491ef6000affb472779dcd22
kidinnu <vadim@mc-kidinnu.local>
2019-03-31 17:36:36 +0400

    Template

--- problems/term_II/dqdt.m
+++ problems/term_II/dqdt.m
@@ -6,6 +6,12 @@ phi  = @(i) q(p.iq(i,1):p.iq(i,1)+p.iq(i,2)-1);
 % get dphi array by hinge index
 dphi = @(i) q(p.iq(i,1)+p.N:p.iq(i,1)+p.iq(i,2)+p.N-1);
 
+% K matrix
+K = zeros(3,3,p.n,p.n);
+% TODO
+%
+%
+% 
 
 % p*T matrix
 pT = zeros(3,p.N,p.n);
@@ -20,7 +26,29 @@ for ib=1:p.n
     end    
 end
 
-dq = pT(1,:,:);
+% p*T*K matrix
+% TODO - Check
+pTK = zeros(3,p.N,p.n);
+for i=1:p.N
+    for j=1:p.n
+        for k=1:p.n
+            pTK(:,i,j) = pTK(:,i,j) + pT(:,i,k)'*K(:,:,k,j);
+        end
+    end
+end
+
+% A matrix
+% TODO - Check
+A = zeros(p.N,p.N);
+for i=1:p.N
+    for j=1:p.N
+        for k=1:p.n
+            A(i,j) = A(i,j) + pTK(:,i,k)*pT(:,j,k);
+        end
+    end
+end
+            
+
 
 
 end
