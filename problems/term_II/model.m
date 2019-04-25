836ca2c0c43910559f9c5028e2e99e1a19c9e7fd
kidinnu <vadim@mc-kidinnu.local>
2019-04-25 21:27:12 +0400

    v0.1

--- problems/term_II/model.m
+++ problems/term_II/model.m
@@ -12,11 +12,11 @@ p.S  = [-1  1  1
          0  0 -1];
 % Joint vectors
 p.C  = zeros(3,p.n,p.n);
-p.C(:,1,1)  = [-1; 0;0];
-p.C(:,1,2)  = [+1; 0;0];
-p.C(:,1,3)  = [+1; 0;0];
-p.C(:,2,2)  = [-1; 0;0];
-p.C(:,3,3)  = [-1; 0;0];
+p.C(:,1,1)  = [0;0;-1];
+p.C(:,1,2)  = [0;0;+1];
+p.C(:,2,3)  = [0;0;+1];
+p.C(:,2,2)  = [0;0;-1];
+p.C(:,3,3)  = [0;0;-1];
 % Masses
 p.mass = [1;1;1];
 % Inertia tensors
@@ -40,9 +40,9 @@ p.A{2} = @(q) Az(q(1))*Ax(q(2));
 p.A{3} = @(q) Ax(q(1));
 % p-vectors
 % 0-1 joint: 3 x 3
-p.p{1} = @(q) [p.A{1}(q)*[0;0;1] p.A{1}([0,q(2),q(3)])*[1;0;0] [0;0;1]];
+p.p{1} = @(q) [p.A{1}(q)'*[0;0;1] p.A{1}([0,q(2),q(3)])'*[1;0;0] [0;0;1]];
 % 1-2 joint: 3 x 2
-p.p{2} = @(q) [p.A{2}(q)*[0;0;1] [1;0;0]];
+p.p{2} = @(q) [p.A{2}(q)'*[0;0;1] [1;0;0]];
 % 2-3 joint: 3 x 1
 p.p{3} = @(q) [1;0;0];
 % Relative angular velocity
@@ -50,8 +50,8 @@ p.Wr{1} = @(q,dq)  p.p{1}(q)*[dq(1);dq(2);dq(3)];
 p.Wr{2} = @(q,dq)  p.p{2}(q)*[dq(1);dq(2)];
 p.Wr{3} = @(q,dq)  p.p{3}(q)*dq;
 % Partial derivatives of p vectors
-p.pw{1} = @(q,dq) sum(tilde(p.Wr{1}(q,dq))*p.p{1}(q),2);
-p.pw{2} = @(q,dq) sum(tilde(p.Wr{2}(q,dq))*p.p{2}(q),2); 
+p.pw{1} = @(q,dq) -cross(p.Wr{1}(q,dq),p.p{1}(q)*[1;0;0])*dq(1)-cross(p.Wr{1}([0;0;0],[0;0;dq(3)]),p.p{1}(q)*[0;1;0])*dq(2); 
+p.pw{2} = @(q,dq) -cross(p.Wr{2}(q,dq),p.p{2}(q)*[1;0])*dq(1); 
 p.pw{3} = @(q,dq) [0;0;0]; 
 % =========================================================================
 % Preprocessing
@@ -63,7 +63,8 @@ preproc;
 % Initial conditions
 q0 = [1;1;1;1;1;1;0;0;0;0;0;0];
 % Start integration process
-[t, q] = ode113(@(t,q) dqdt(t,q,p), [0 1.3], q0);
+[t, q] = ode113(@(t,q) dqdt(t,q,p), [0 5], q0);
+fprintf('OK\n');
 %% =========================================================================
 % Postprocessing
 % =========================================================================
@@ -74,9 +75,6 @@ plot(t, q(:,4:5));legend('\phi_{21}','\phi_{22}');xlabel('t, c');
 subplot(313);
 plot(t, q(:,6));legend('\phi_{31}');xlabel('t, c');
 % Checking conservation of the energy
-%
-%
-%
 
 % Export data for animation 
 %
