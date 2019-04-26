1467d749fa951ed598f7cce367a9c11d9901c84d
kidinnu <vadim@mc-kidinnu.local>
2019-04-26 06:39:45 +0400

    Partial derivatives of p vectors are corrected

--- problems/term_II/model.m
+++ problems/term_II/model.m
@@ -50,8 +50,10 @@ p.Wr{1} = @(q,dq)  p.p{1}(q)*[dq(1);dq(2);dq(3)];
 p.Wr{2} = @(q,dq)  p.p{2}(q)*[dq(1);dq(2)];
 p.Wr{3} = @(q,dq)  p.p{3}(q)*dq;
 % Partial derivatives of p vectors
-p.pw{1} = @(q,dq) -cross(p.Wr{1}(q,dq),p.p{1}(q)*[1;0;0])*dq(1)-cross(p.Wr{1}([0;0;0],[0;0;dq(3)]),p.p{1}(q)*[0;1;0])*dq(2); 
-p.pw{2} = @(q,dq) -cross(p.Wr{2}(q,dq),p.p{2}(q)*[1;0])*dq(1); 
+p.pw{1} = @(q,dq) [cos(q(2))*sin(q(3))*dq(2)+cos(q(3))*sin(q(2))*dq(3);
+                   cos(q(2))*cos(q(3))*dq(2)-sin(q(3))*sin(q(2))*dq(3);
+                   -sin(q(2))*q(2)]+[-sin(q(3));-cos(q(3));0]*dq(3);
+p.pw{2} = @(q,dq) [0;cos(q(2))*dq(2);-sin(q(2))*q(2)]; 
 p.pw{3} = @(q,dq) [0;0;0]; 
 % =========================================================================
 % Preprocessing
@@ -63,7 +65,7 @@ preproc;
 % Initial conditions
 q0 = [1;1;1;1;1;1;0;0;0;0;0;0];
 % Start integration process
-[t, q] = ode113(@(t,q) dqdt(t,q,p), [0 5], q0);
+[t, q] = ode113(@(t,q) dqdt(t,q,p), [0 2], q0);
 fprintf('OK\n');
 %% =========================================================================
 % Postprocessing
